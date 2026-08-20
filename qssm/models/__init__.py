b1da0686d821760d826067ed9e3ef4ce2a4d1722
Moona-Mazher <75270567+Moona-Mazher@users.noreply.github.com>
2026-08-20 15:44:26 +0100

    Update __init__.py

--- qssm/models/__init__.py
+++ qssm/models/__init__.py
@@ -2,3 +2,5 @@ from .patch_embed_3d import PatchEmbed3D
 from .qssm_block import QSSMBlock
 from .encoder_3d import QSSM3DEncoder
 from .mae_3d import QSSM3DMAE
+from .brain_age_head import BrainAgeHead
+from .brain_age_model import QSSM3DBrainAge
